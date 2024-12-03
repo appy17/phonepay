8512930b264953abd56a2e3dda230ac7fec5902c
Apeksha Tembhurne <appy17@users.noreply.github.com>
2024-12-03 11:19:18 +0530

    hey

--- server.js
+++ server.js
@@ -108,6 +108,11 @@ app.post("/status", async (req, res) => {
   });
 });
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+// app.listen(8000, () => {
+//   console.log("Server is running on port 8000");
+// });
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
